af6773411ca462493f98f1676dcf5c32b6d33795
jogeix_jb <jean-baptiste.jogeix@ynov.com>
2017-03-29 14:10:25 +0200

    implementing serverConnect dbConnect and getDatabases

--- MySQLBackup.py
+++ MySQLBackup.py
@@ -1,35 +1,87 @@
 from IBackup import IBackup
 from pathlib import Path
+from functions import displayError
+import pymysql
 import os, time
 
-"""
-Classe permettant de gérer la sauvegarde à une base de donnée MySQL
-Contient un attribut de type BackupOption contenant les paramètres nécessaires (CF BackupOption)
-"""
 class MySQLBackup(IBackup):
+    """
+    Classe permettant de gérer la sauvegarde à une base de donnée MySQL
+    Contient un attribut de type BackupOption contenant les paramètres nécessaires (CF BackupOption)
+    """
 
-    """ Constructeur qui initialise l'attribut options """
     def __init__(self, options):
+        """ Constructeur qui initialise l'attribut options """
         self.options = options
 
-    # TODO à développer
     def serverConnect(self):
-        pass
-
-    # TODO à développer
-    def DBConnect(self, DbName):
-        pass
+        """
+        Retourne une connexion au SGBD
+        si une erreur se produit, affiche le message et renvoie None
+        """
+        try:
+            return pymysql.connect(host=self.options.host, user=self.options.user, password=self.options.pwd,
+                charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        except pymysql.err.OperationalError as e1:
+            if(e1.args[0]==2003):
+                displayError("Impossible de se connecter au serveur")
+            elif(e1.args[0]==1045):
+                displayError("Acces refusé! Vous ne disposez pas des droits nécessaires d'accès au serveur ")
+            else:   
+                displayError("Error " +str(e1.args[0])+"\n"+e1.args[1])
+            return None
+        
+    def dbConnect(self, dbName):     
+        """
+        Retourne une connexion à la bdd spécifiée du SGBD en option
+        si une erreur se produit, affiche le message et renvoie None
+        """   
+        try:
+            return pymysql.connect(host=self.options.host, user=self.options.user, password=self.options.pwd,
+                 db=dbName,charset='utf8mb4',cursorclass=pymysql.cursors.DictCursor)
+        except pymysql.err.OperationalError as e1:
+            if(e1.args[0]==1044):
+                displayError("Acces refusé! Vous ne disposez pas des droits nécessaires d'accès à la base")
+            else:   
+                displayError("Error " +str(e1.args[0])+"\n"+e1.args[1])        
+        except pymysql.err.InternalError as e2:
+            if(e2.args[0]==1049):
+                displayError("La base de données n'éxiste pas")
+            else:   
+                displayError("Error " +str(e2.args[0])+"\n"+e2.args[1])
+        except Exception as e3:
+            #pour le debugage et retour utilisateur
+            displayError("Error " +str(e3.args[0])+"\n"+e3.args[1])
+        return None   
 
     def getOptions(self):
         return self.options
-
-    '''
-    Exécute la sauvegarde en fonction des options
-    Et créé un dossier si inexistant par BDD, ainsi qu'un fichier dump MySql pour chaque base de données
-    '''
+    
+    def getDatabases(self):
+        """ 
+        Retourne un tableau de string contenant la liste des bdd. 
+        Retourne None si une erreur de connexion se produit
+        """
+        try:
+            res=[]
+            connection = self.serverConnect()
+            cursor = connection.cursor()
+            cursor.execute('SHOW DATABASES')
+            database = cursor.fetchone()
+            connection.close()
+            while database:
+                res.append(database['Database'])
+                database = cursor.fetchone()
+            return res
+        except Exception as e:
+            return None
 
     # TODO A redéfinir
-    def execute(self): # utiliser l'objet option pour récupérer les paramètres
+    def execute(self): # utiliser l'objet option pour récupérer les paramètres            
+        """
+        Exécute la sauvegarde en fonction des options
+        Et créé un dossier si inexistant par BDD, ainsi qu'un fichier dump MySql pour chaque base de données
+        """
         counter = 0
         while counter < len(DBList):
 
@@ -60,4 +112,4 @@ class MySQLBackup(IBackup):
                 print("La base de donnée n'existe pas ...")
                 delWrongDb(backupFilePath)
             counter += 1
-
+        
